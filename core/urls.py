e4217b555f0d92b2b2fd10abcdc670fa5fc17d9f
YuroslavPM <yuroslavpm11@gmail.com>
2026-02-18 17:53:31 +0200

    Add login

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from rest_framework.authtoken.views import obtain_auth_token 
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api', include('teams_management.urls')),
+    path('api/login/', obtain_auth_token, name='api_token_auth')
 ]

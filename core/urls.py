c024b59ed2320964473d7e62672b7f9b30302279
YuroslavPM <yuroslavpm11@gmail.com>
2026-03-19 09:44:42 +0200

    Rename keyword for auth to Bearer

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('teams_management.urls')),
-    path('api/login/', obtain_auth_token, name='api_token_auth')
+    path('api/login', obtain_auth_token, name='api_token_auth')
 ]
